d5046bbda9d4c8f85167e488cbe1fb6a6337c273
colibri-the-bird <120297368+colibri-the-bird@users.noreply.github.com>
2024-04-09 15:37:32 +0500

    dfghgvgouuop

---
+++ Assets/Brige.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brige : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
